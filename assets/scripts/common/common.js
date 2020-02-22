4d0b30cbd655bdec032dd98f9571614ecb01eb68
georgezeng <zzeng@bglcorp.com.au>
2020-02-22 14:16:22 +0800

    no message

--- assets/scripts/common/common.js
+++ assets/scripts/common/common.js
@@ -1,3 +1,183 @@
+var cards = {};
+cc.loader.loadRes('AS', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['AS'] = spriteFrame;
+}); 
+cc.loader.loadRes('AH', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['AH'] = spriteFrame;
+});
+cc.loader.loadRes('AC', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['AC'] = spriteFrame;
+}); 
+cc.loader.loadRes('AD', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['AD'] = spriteFrame;
+});
+
+cc.loader.loadRes('2S', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['2S'] = spriteFrame;
+}); 
+cc.loader.loadRes('2H', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['2H'] = spriteFrame;
+});
+cc.loader.loadRes('2C', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['2C'] = spriteFrame;
+}); 
+cc.loader.loadRes('2D', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['2D'] = spriteFrame;
+});
+
+cc.loader.loadRes('3S', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['3S'] = spriteFrame;
+}); 
+cc.loader.loadRes('3H', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['3H'] = spriteFrame;
+});
+cc.loader.loadRes('3C', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['3C'] = spriteFrame;
+}); 
+cc.loader.loadRes('3D', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['3D'] = spriteFrame;
+});
+
+cc.loader.loadRes('4S', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['4S'] = spriteFrame;
+}); 
+cc.loader.loadRes('4H', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['4H'] = spriteFrame;
+});
+cc.loader.loadRes('4C', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['4C'] = spriteFrame;
+}); 
+cc.loader.loadRes('4D', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['4D'] = spriteFrame;
+});
+
+cc.loader.loadRes('5S', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['5S'] = spriteFrame;
+}); 
+cc.loader.loadRes('5H', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['5H'] = spriteFrame;
+});
+cc.loader.loadRes('5C', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['5C'] = spriteFrame;
+}); 
+cc.loader.loadRes('5D', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['5D'] = spriteFrame;
+});
+
+cc.loader.loadRes('6S', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['6S'] = spriteFrame;
+}); 
+cc.loader.loadRes('6H', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['6H'] = spriteFrame;
+});
+cc.loader.loadRes('6C', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['6C'] = spriteFrame;
+}); 
+cc.loader.loadRes('6D', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['6D'] = spriteFrame;
+});
+
+cc.loader.loadRes('7S', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['7S'] = spriteFrame;
+}); 
+cc.loader.loadRes('7H', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['7H'] = spriteFrame;
+});
+cc.loader.loadRes('7C', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['7C'] = spriteFrame;
+}); 
+cc.loader.loadRes('7D', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['7D'] = spriteFrame;
+});
+
+cc.loader.loadRes('8S', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['8S'] = spriteFrame;
+}); 
+cc.loader.loadRes('8H', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['8H'] = spriteFrame;
+});
+cc.loader.loadRes('8C', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['8C'] = spriteFrame;
+}); 
+cc.loader.loadRes('8D', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['8D'] = spriteFrame;
+});
+
+cc.loader.loadRes('9S', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['9S'] = spriteFrame;
+}); 
+cc.loader.loadRes('9H', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['9H'] = spriteFrame;
+});
+cc.loader.loadRes('9C', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['9C'] = spriteFrame;
+}); 
+cc.loader.loadRes('9D', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['9D'] = spriteFrame;
+});
+
+cc.loader.loadRes('10S', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['10S'] = spriteFrame;
+}); 
+cc.loader.loadRes('10H', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['10H'] = spriteFrame;
+});
+cc.loader.loadRes('10C', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['10C'] = spriteFrame;
+}); 
+cc.loader.loadRes('10D', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['10D'] = spriteFrame;
+});
+
+cc.loader.loadRes('JS', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['JS'] = spriteFrame;
+}); 
+cc.loader.loadRes('JH', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['JH'] = spriteFrame;
+});
+cc.loader.loadRes('JC', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['JC'] = spriteFrame;
+}); 
+cc.loader.loadRes('JD', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['JD'] = spriteFrame;
+});
+
+cc.loader.loadRes('QS', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['QS'] = spriteFrame;
+}); 
+cc.loader.loadRes('QH', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['QH'] = spriteFrame;
+});
+cc.loader.loadRes('QC', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['QC'] = spriteFrame;
+}); 
+cc.loader.loadRes('QD', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['QD'] = spriteFrame;
+});
+
+cc.loader.loadRes('KS', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['KS'] = spriteFrame;
+}); 
+cc.loader.loadRes('KH', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['KH'] = spriteFrame;
+});
+cc.loader.loadRes('KC', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['KC'] = spriteFrame;
+}); 
+cc.loader.loadRes('KD', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['KD'] = spriteFrame;
+});
+
+cc.loader.loadRes('JK1', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['JK1'] = spriteFrame;
+});
+cc.loader.loadRes('JK2', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['JK2'] = spriteFrame;
+});
+cc.loader.loadRes('back', cc.SpriteFrame, function(err, spriteFrame) {
+	cards['back'] = spriteFrame;
+});
+
 var common = {
     getValue: function (key) {
         let value = cc.sys.localStorage.getItem(key);
@@ -10,7 +190,7 @@ var common = {
 	
 	loadCardImg(sprite, cardName) {
 		cc.loader.loadRes(cardName, cc.SpriteFrame, function(err, spriteFrame) {
-			sprite.spriteFrame = spriteFrame;
+			sprite.spriteFrame = cards[cardName];
 			sprite.node.opacity = 255;
 		}); 
 	}
